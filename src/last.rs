a1f7407ed15c35676e5d78e0017167c142fb0832
dfhoughton <dfhoughton@gmail.com>
2019-12-29 19:40:52 -0500

    last subcommand working

--- src/last.rs
+++ src/last.rs
@@ -1,7 +1,11 @@
 extern crate clap;
 extern crate regex;
+extern crate chrono;
 
-use crate::util::common_search_or_filter_arguments;
+use crate::configure::Configuration;
+use crate::log_items::{Event, Filter, LogReader, Note};
+use crate::util::{common_search_or_filter_arguments, display_events, display_notes};
+use chrono::Local;
 use clap::{App, ArgMatches, SubCommand};
 
 pub fn cli(mast: App<'static, 'static>) -> App<'static, 'static> {
@@ -18,5 +22,34 @@ pub fn cli(mast: App<'static, 'static>) -> App<'static, 'static> {
 }
 
 pub fn run(matches: &ArgMatches) {
-    println!("last");
+    let filter = Filter::new(matches);
+    let reader = LogReader::new(None).expect("could not read log");
+    let configuration = Configuration::read();
+    if matches.is_present("notes") {
+        let note: Vec<Note> = reader
+            .notes_from_the_end()
+            .filter(|n| filter.matches(n))
+            .take(1)
+            .collect();
+        if note.is_empty() {
+            println!("no note found")
+        } else {
+            let start = &note[0].time.clone();
+            let now = Local::now().naive_local();
+            display_notes(note, start, &now, &configuration);
+        }
+    } else {
+        let event: Vec<Event> = reader
+            .events_from_the_end()
+            .filter(|n| filter.matches(n))
+            .take(1)
+            .collect();
+        if event.is_empty() {
+            println!("no event found")
+        } else {
+            let start = &event[0].start.clone();
+            let now = Local::now().naive_local();
+            display_events(event, start, &now, &configuration);
+        }
+    }
 }
