48f6fd50300c83e6d599b7c46c179c8899ce4ccf
dfhoughton <dfhoughton@gmail.com>
2020-01-12 19:24:45 -0500

    refactoring to facilitate testing

--- src/edit.rs
+++ src/edit.rs
@@ -4,12 +4,13 @@ extern crate clap;
 use crate::configure::Configuration;
 use crate::log::{parse_line, timestamp, Item};
 use crate::util::{base_dir, fatal, log_path, warn};
-use chrono::{Local, NaiveDateTime};
+use chrono::{Local, NaiveDateTime, NaiveDate, Duration};
 use clap::{App, Arg, ArgMatches, SubCommand};
 use std::fs::{copy, File};
 use std::io::{BufRead, BufReader, BufWriter, Read, Write};
 use std::path::PathBuf;
 use std::process::Command;
+use std::str::FromStr;
 
 pub fn cli(mast: App<'static, 'static>) -> App<'static, 'static> {
     mast.subcommand(
@@ -30,29 +31,30 @@ pub fn cli(mast: App<'static, 'static>) -> App<'static, 'static> {
 pub fn run(matches: &ArgMatches) {
     let conf = Configuration::read(None);
     if matches.is_present("validate") {
-        validation_messages(0, 0, &conf);
+        validation_messages(0, 0, &conf, None, None);
     } else {
         if let Some((editor, _)) = conf.effective_editor() {
-            copy(log_path(), backup()).expect("could not make backup log");
+            let backed_up_backup = backup_backup();
+            copy(log_path(), backup(None)).expect("could not make backup log");
             let status = Command::new(&editor)
                 .arg(log_path().to_str().unwrap())
                 .status()
                 .expect("failed to start editor process");
             if status.success() {
-                if let Some((offset, line_number)) = find_change_offset() {
-                    validation_messages(offset, line_number, &conf);
+                if let Some((offset, line_number)) = find_change_offset(None, None) {
+                    validation_messages(offset, line_number, &conf, None, None);
                 } else {
                     println!("no change found in log file; deleting backup...");
-                    std::fs::remove_file(backup()).expect("failed to remove log.bak");
+                    restore_backup(backed_up_backup);
                 }
             } else {
                 fatal(
                     "the editor closed with an error; restoring log file from backup",
                     &conf,
                 );
-                copy(backup(), log_path()).expect("could restore log from backup");
+                copy(backup(None), log_path()).expect("could not restore log from backup");
+                restore_backup(backed_up_backup);
                 println!("done");
-                std::fs::remove_file(backup()).expect("failed to remove log.bak");
             }
         } else {
             fatal(
@@ -63,11 +65,33 @@ pub fn run(matches: &ArgMatches) {
     }
 }
 
+fn restore_backup(backed_up_backup: bool) {
+    std::fs::remove_file(backup(None)).expect("failed to remove log.bak");
+    if backed_up_backup {
+        copy(backup_backup_file(), backup(None))
+            .expect("could not restore pre-existing backup file");
+        std::fs::remove_file(backup_backup_file())
+            .expect("could not removed backup of backup file");
+    }
+}
+
+// backup the backup if it exists and return whether you did so
+fn backup_backup() -> bool {
+    if log_path().as_path().exists() {
+        copy(log_path(), backup_backup_file()).expect("could not make backup log");
+        true
+    } else {
+        false
+    }
+}
+
 // scan for first line that differs
-fn find_change_offset() -> Option<(usize, usize)> {
-    let edited = File::open(log_path()).expect("could not open edited log file for reading");
+// returns byte count and line count
+fn find_change_offset(log: Option<&str>, backup_file: Option<&str>) -> Option<(usize, usize)> {
+    let edited = File::open(log_file(log)).expect("could not open edited log file for reading");
     let mut edited = BufReader::new(edited);
-    let backup = File::open(backup()).expect("could not backup log file to check for changes");
+    let backup =
+        File::open(backup(backup_file)).expect("could not backup log file to check for changes");
     let mut backup = BufReader::new(backup);
     let mut buf1 = String::new();
     let mut buf2 = String::new();
@@ -94,21 +118,52 @@ fn find_change_offset() -> Option<(usize, usize)> {
     None
 }
 
-// backup file
-fn backup() -> PathBuf {
+// backup log file
+fn backup(file: Option<&str>) -> PathBuf {
+    if let Some(file) = file {
+        PathBuf::from_str(file).expect(&format!("could not create path from {}", file))
+    } else {
+        let mut backup = base_dir();
+        backup.push("log.bak");
+        backup
+    }
+}
+
+// a backup of the backup in case (this should get cleaned up at the end of the process)
+fn backup_backup_file() -> PathBuf {
     let mut backup = base_dir();
-    backup.push("log.bak");
+    backup.push("log.bak.bak");
     backup
 }
 
-fn validation_file() -> PathBuf {
-    let mut validation_file_path = base_dir();
-    validation_file_path.push("log.validation");
-    validation_file_path
+fn validation_file(file: Option<&str>) -> PathBuf {
+    if let Some(file) = file {
+        PathBuf::from_str(file).expect(&format!("could not create path from {}", file))
+    } else {
+        let mut validation_file_path = base_dir();
+        validation_file_path.push("log.validation");
+        validation_file_path
+    }
 }
 
-fn validation_messages(byte_offset: usize, starting_line: usize, conf: &Configuration) {
-    if let Some((line_number, count)) = validate(byte_offset, starting_line) {
+fn log_file(file: Option<&str>) -> PathBuf {
+    if let Some(path) = file {
+        PathBuf::from_str(path).expect(&format!("could not create a path with {}", path))
+    } else {
+        log_path()
+    }
+}
+
+fn validation_messages(
+    byte_offset: usize,
+    starting_line: usize,
+    conf: &Configuration,
+    log: Option<&str>,
+    validation_file_name: Option<&str>,
+) {
+    if let Some((line_number, count)) =
+        validate(byte_offset, starting_line, log, validation_file_name)
+    {
         if count > 1 {
             warn(
                 format!(
@@ -120,18 +175,25 @@ fn validation_messages(byte_offset: usize, starting_line: usize, conf: &Configur
         } else {
             warn(format!("one error was found at line {}", line_number), conf)
         }
-        copy(validation_file(), log_path()).expect("could not copy validation file to log");
-        std::fs::remove_file(validation_file()).expect("could not remove validation file");
+        copy(validation_file(validation_file_name), log_file(log))
+            .expect("could not copy validation file to log");
+        std::fs::remove_file(validation_file(validation_file_name))
+            .expect("could not remove validation file");
     } else {
         println!("log is valid")
     }
 }
 
 // returns line number and error count
-fn validate(byte_offset: usize, starting_line: usize) -> Option<(usize, usize)> {
-    let edited = File::open(log_path()).expect("could not open edited log file for reading");
+fn validate(
+    byte_offset: usize,
+    starting_line: usize,
+    log: Option<&str>,
+    validation: Option<&str>,
+) -> Option<(usize, usize)> {
+    let edited = File::open(log_file(log)).expect("could not open edited log file for reading");
     let mut reader = BufReader::new(edited);
-    let validation_file = File::create(validation_file().as_path())
+    let validation_file = File::create(validation_file(validation).as_path())
         .expect("could not open file to receive validation output");
     let mut writer = BufWriter::new(validation_file);
     let mut bytes_written: usize = 0;
@@ -203,9 +265,14 @@ fn validate(byte_offset: usize, starting_line: usize) -> Option<(usize, usize)> 
             if error_count == 1 {
                 first_error = line_number + 1;
             }
-            let bytes: Vec<u8> = format!("# ERROR {} on {}: {}\n# ", error_count, now.format("%F at %r"), msg)
-                .bytes()
-                .collect();
+            let bytes: Vec<u8> = format!(
+                "# ERROR {} on {}: {}\n# ",
+                error_count,
+                now.format("%F at %r"),
+                msg
+            )
+            .bytes()
+            .collect();
             writer
                 .write_all(&bytes)
                 .expect("failed to write error message to validation file");
@@ -221,5 +288,91 @@ fn validate(byte_offset: usize, starting_line: usize) -> Option<(usize, usize)> 
         Some((first_error, error_count))
     } else {
         None
+    }
+}
+
+// find_change_offset(log: Option<&str>, backup_file: Option<&str>)
+// validation_messages(byte_offset: usize, starting_line: usize, conf: &Configuration, log: Option<&str>, validation_file_name: Option<&str>)
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::log::{Event, LogLine};
+
+    fn create_log<T: ToString>(disambiguator: &str, lines: &[T]) -> (String, PathBuf) {
+        let disambiguator = format!("log_{}", disambiguator);
+        let log_buffer = PathBuf::from_str(&disambiguator)
+            .expect(&format!("could not create log file for {}", disambiguator));
+        let file = File::create(log_buffer.as_path())
+            .expect("could not open file to receive validation output");
+        let mut writer = BufWriter::new(file);
+        for line in lines {
+            let line = line.to_string();
+            writeln!(writer, "{}", line).expect(&format!(
+                "faile to write {} into log_{}",
+                line, disambiguator
+            ));
+        }
+        (disambiguator, log_buffer)
+    }
+
+    fn cleanup(files: Vec<PathBuf>) {
+        for pb in files {
+            if pb.as_path().exists() {
+                std::fs::remove_file(pb).expect("could not cleanup file");
+            }
+        }
+    }
+
+    fn configuration_path(disambiguator: &str) -> PathBuf {
+        PathBuf::from_str(&format!("{}_configuration", disambiguator)).expect("could not make path")
+    }
+
+    #[test]
+    fn test_find_change_offset_when_no_change() {
+        let disambiguator1 = "test_find_change_offset_when_no_change1";
+        let disambiguator2 = "test_find_change_offset_when_no_change2";
+        let lines = ["foo", "bar", "baz"];
+        let (n1, log1) = create_log(disambiguator1, &lines);
+        let (n2, log2) = create_log(disambiguator2, &lines);
+        let diff = find_change_offset(Some(&n1), Some(&n2));
+        assert!(diff.is_none(), "no difference found");
+        cleanup(vec![log1, log2]);
+    }
+
+    #[test]
+    fn test_find_change_offset_with_change() {
+        let disambiguator1 = "test_find_change_offset_with_change1";
+        let disambiguator2 = "test_find_change_offset_with_change2";
+        let (n1, log1) = create_log(disambiguator1, &["foo", "bar", "baz"]);
+        let (n2, log2) = create_log(disambiguator2, &["foo", "bar"]);
+        let diff = find_change_offset(Some(&n1), Some(&n2));
+        println!("{:?}", diff);
+        assert!(diff.is_some(), "difference found");
+        assert_eq!(2, diff.unwrap().1, "difference at third line");
+        cleanup(vec![log1, log2]);
+    }
+
+    #[test]
+    fn test_validation_messages_all_good() {
+        let disambiguator = "test_validation_messages_all_good";
+        let validation = format!("validation_{}", disambiguator);
+        let validation_path = PathBuf::from_str(&validation).expect("could not make path");
+        let conf_path = configuration_path(disambiguator);
+        let conf = Configuration::read(Some(conf_path));
+        let mut t = NaiveDate::from_ymd(2000, 1, 1).and_hms(0, 0, 0);
+        let mut events = vec![];
+        for duration in &[2, 1, 3] {
+            let end_time = t + Duration::hours(*duration as i64);
+            let mut event = Event::coin(format!("event {}", duration), vec![]);
+            event.start = t.clone();
+            event.end = Some(end_time.clone());
+            t = end_time;
+            events.push(event.to_line());
+        }
+        let (name, buff) = create_log(disambiguator, &events);
+        let backup_name = format!("{}.bak", disambiguator);
+        let(backup_name, backup_buff) = create_log(&backup_name, &events);
+        validation_messages(0, 0, &conf, Some(&name), Some(&backup_name));
+        cleanup(vec![buff, backup_buff, configuration_path(disambiguator), validation_path]);
     }
 }
